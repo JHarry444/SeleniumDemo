c01499b89050f95e7beefb42e2639d77fd179a73
Jordan <jordan.harrison@qa.com>
2020-06-09 09:33:14 +0100

    added extent reports

--- src/test/java/com/qa/test/SeleniumTest.java
+++ src/test/java/com/qa/test/SeleniumTest.java
@@ -2,27 +2,49 @@ package com.qa.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.qa.test.pages.BingLandingPage;
 import com.qa.test.pages.BingSearchPage;
 
 public class SeleniumTest {
 
-	WebDriver driver;
+	private WebDriver driver;
+
+	private static ExtentReports report;
+
+	private ExtentTest test;
+
+	@BeforeClass
+	@SuppressWarnings("deprecation")
+	public static void setup() {
+		report = new ExtentReports();
+		ExtentHtmlReporter htmlReport = new ExtentHtmlReporter("test-output/html/extentReport.html");
+		htmlReport.config().setAutoCreateRelativePathMedia(true);
+		report.attachReporter(htmlReport);
+	}
 
 	@Before
 	public void init() {
-		driver = new ChromeDriver();
+		ChromeOptions opts = new ChromeOptions();
+		opts.setHeadless(true);
+		driver = new ChromeDriver(opts);
 		driver.manage().window().maximize();
 	}
 
@@ -44,6 +66,7 @@ public class SeleniumTest {
 	@Test
 	public void testPOM() {
 		driver.get("https://www.bing.co.uk");
+		this.test = report.createTest("testPOM");
 
 		final String searchTerm = "turtles";
 
@@ -52,7 +75,12 @@ public class SeleniumTest {
 
 		BingSearchPage searchPage = PageFactory.initElements(driver, BingSearchPage.class);
 
-		assertEquals(searchTerm, searchPage.getSearchTerm());
+		if (searchPage.getSearchTerm().equals(searchTerm)) {
+			test.pass("Correct search term found.");
+		} else {
+			test.fail("Correct search term not found.");
+			fail();
+		}
 	}
 
 	@Test
@@ -97,5 +125,10 @@ public class SeleniumTest {
 	@After
 	public void tearDown() {
 		driver.quit();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		report.flush();
 	}
 }
